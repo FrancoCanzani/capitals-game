c0cb819b2e284b06be75775332137fb85235493e
Franco Canzani <francocanzani@gmail.com>
2023-08-30 00:04:30 +0200

    Switch signInWithPopup to redirect

--- utils/signInWithGoogle.ts
+++ utils/signInWithGoogle.ts
@@ -1,30 +1,9 @@
-import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  UserCredential,
-} from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import { app } from '../firebase';
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
 
 export default async function signInWithGoogle(): Promise<void> {
-  try {
-    const result: UserCredential = await signInWithPopup(auth, provider);
-
-    // This gives you the signed-in user info.
-    const user = result.user;
-
-    // Handle Google Access Token if needed.
-    const accessToken: string | undefined = user?.refreshToken;
-    if (accessToken) {
-      // You can use the accessToken here.
-    }
-  } catch (error: any) {
-    // Handle authentication errors here.
-    const errorCode: string = error.code;
-    const errorMessage: string = error.message;
-    console.error(errorCode, errorMessage);
-  }
+  signInWithRedirect(auth, provider);
 }
