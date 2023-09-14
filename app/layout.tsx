2e5bfbdaf15053491a38d851b659ac47ea516049
Franco Canzani <francocanzani@gmail.com>
2023-09-14 17:58:16 +0200

    Adds vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import Footer from "./components/footer";
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         className={`${inter.className} relative flex min-h-screen min-w-full flex-col items-center rounded-md bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] antialiased dark:bg-slate-800`}
       >
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
