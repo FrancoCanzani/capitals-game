cc7249375cd5c51c3a66cd5e0e72ae694c457b1b
Franco Canzani <francocanzani@gmail.com>
2023-09-16 20:38:38 +0200

    Adds toaster to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import ClientProvider from "./components/clientProvider";
 import getCountries from "../utils/getCountries";
+import { Toaster } from "sonner";
 
 export default async function Home() {
   const countries = await getCountries();
 
-  return <ClientProvider countries={countries} />;
+  return (
+    <>
+      <Toaster theme="system" />
+      <ClientProvider countries={countries} />;
+    </>
+  );
 }
