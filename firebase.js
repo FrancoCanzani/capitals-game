4b26623b30b2c3578ca07cda6894abcaa77ea7ad
Franco Canzani <francocanzani@gmail.com>
2023-09-16 15:31:51 +0200

    Adds firebase authDomain to config

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBcEuqLpZsY80t90W3OcXCkmK0Vq1fI6kM",
-  authDomain: "auth.guessthecapital.com",
+  authDomain: "guessthecapital.com",
   projectId: "capitals-game-43aa4",
   storageBucket: "capitals-game-43aa4.appspot.com",
   messagingSenderId: "313370284240",
